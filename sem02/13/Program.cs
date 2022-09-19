294acfcc28d17722832c6e8d8f50830dddebad46
zayha <5350061@mail.ru>
2022-09-19 23:13:17 +0300

    add task 13

---
+++ sem02/13/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Please enter some number: ");
+int number =  int.Parse(Console.ReadLine()!);
+string numIndex = number.ToString();
+
+if (numIndex.Length > 2) {
+    Console.WriteLine($"The third digit in the number = {numIndex[2]}");
+}
+else {
+    Console.WriteLine("The third digit is missing...");
+}
