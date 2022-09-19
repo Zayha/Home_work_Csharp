4673ae623429109f009153e3a97e8f93bb46a41f
zayha <5350061@mail.ru>
2022-09-19 22:58:23 +0300

    add task 10

---
+++ sem02/10/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Please enter a three-digit number: ");
+int number =  int.Parse(Console.ReadLine()!);
+
+if (99 < number && number < 1000) {
+    string numIndex = number.ToString();
+    Console.WriteLine($"{numIndex[1]}");
+} 
+else {
+    Console.WriteLine($"Number {number} not three-digit!");
+}
