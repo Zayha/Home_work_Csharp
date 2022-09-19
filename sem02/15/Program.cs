8682e2996ea38c8747b617e0b21a7daa26531863
zayha <5350061@mail.ru>
2022-09-19 23:35:06 +0300

    add task 15

---
+++ sem02/15/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+Console.Write("Please enter day number: ");
+int dayNumber =  int.Parse(Console.ReadLine()!);
+
+var days = new Dictionary<int, string>()
+{
+    {1, "Monday"},
+    {2, "Tuesday"},
+    {3, "Wednesday"},
+    {4, "Thursday"},
+    {5, "Friday"},
+    {6, "Saturday"},
+    {0, "Sunday"}
+};
+
+if (dayNumber != 0) {
+    Console.WriteLine($"The day {dayNumber} - {days[(dayNumber % 7)]}");
+}
+else {
+    Console.WriteLine("And there was nothing yet on day zero!");
+}
