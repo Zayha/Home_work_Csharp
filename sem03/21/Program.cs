dd7faa9d4b35d16b5f4d1d81d238a9f1ccd8a58d
zayha <5350061@mail.ru>
2022-09-24 11:13:19 +0300

    add task 21 and 23 code

---
+++ sem03/21/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+Console.Write("Введите Координату первой точки через пробел пример - \"1 2 3\" : ");
+int[] d1 = Console.ReadLine()!.Split(' ').Select(e => Convert.ToInt32(e)).ToArray();
+Console.Write("Введите Координату второй точки через пробел пример - \"1 2 3\" : ");
+int[] d2 = Console.ReadLine()!.Split(' ').Select(e => Convert.ToInt32(e)).ToArray();
+
+double dv = Math.Sqrt(Math.Pow(d1[0] - d2[0], 2) + Math.Pow(d1[1] - d2[1], 2) + Math.Pow(d1[2] - d2[2], 2));
+Console.WriteLine($"Рассточние между точкой с координатами {d1[0]} {d1[1]} {d1[2]} и точкой {d2[0]} {d2[1]} {d2[2]} составляет {dv.ToString("#.00")}");
---
+++ sem03/23/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int number = Math.Abs(int.Parse(Console.ReadLine()!));
+
+int counter = 1;
+
+Console.Write($"{number} -> ");
+while (counter <= number )
+{
+    Console.Write(Math.Pow(counter, 3));
+    if (counter < number)
+    {
+        Console.Write($", ");
+    }
+    counter++;
+}
