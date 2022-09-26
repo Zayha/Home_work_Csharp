f61162637d4a8a7d0f78fce9446fa3a7b9bb9715
zayha <5350061@mail.ru>
2022-09-27 00:09:57 +0300

    add task 29

---
+++ sem04/29/Program.cs
@@ -0,0 +1,27 @@
+﻿
+int[] CreateArray(int l = 8)
+{
+    int[] array = new int[l];
+    var random = new Random();
+    for (int i = 0; i < l; i++)
+    {
+        array[i] = random.Next();
+    }
+    return array;
+}
+
+void ShowArray(int[] array)
+{   Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i < array.Length - 1)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.Write("]");
+}
+
+Console.Clear();
+ShowArray(CreateArray());
