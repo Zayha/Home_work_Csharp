cabe46e18989fa1a9bf6b4052d34ed98415da6db
zayha <5350061@mail.ru>
2022-09-26 23:46:20 +0300

    add task 27

---
+++ sem04/27/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int number = Math.Abs(int.Parse(Console.ReadLine()!));
+
+int SumDigits(int number)
+{
+    string line = number.ToString();
+    int sum = 0;
+    for (int i = 0; i < line.Length; i++)
+    {
+        sum = sum + int.Parse(line[i].ToString());
+    }
+    return sum;
+}
+
+Console.WriteLine($"{number} -> {SumDigits(number)}");
