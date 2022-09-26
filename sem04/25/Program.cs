b178575766e97d0084c4b6e2d1d1ab7d439bed47
zayha <5350061@mail.ru>
2022-09-26 23:17:16 +0300

    add task 25

---
+++ sem04/25/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+Console.Write("Введите число A: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число B: ");
+int b = int.Parse(Console.ReadLine()!);
+
+
+int Power(int a, int b)
+{   
+    int ch = 1;
+    for (int i = 1; i <= b; i++)
+    {
+        ch = ch * a;
+    }
+    return ch;
+}
+
+Console.Write($"Число {a}, возведенное в степень {b} = {Power(a, b)}");
