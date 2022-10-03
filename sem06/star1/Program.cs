544d92b00f2f906e19a77410e675236c04a63ca7
zayha <5350061@mail.ru>
2022-10-04 00:56:11 +0300

    add task star1

---
+++ sem06/star1/Program.cs
@@ -0,0 +1,33 @@
+﻿//В контексте задачи, отрицательные числа не рассматривались
+Console.Clear();
+Console.Write("Введите число в десятичной системе исчисления: ");
+int m = int.Parse(Console.ReadLine()!);
+Dectobin(m);
+
+void Dectobin(int dec, int f = 1, string s = "")
+{   
+    if (((dec == 1) || (dec == 0)) && (f == 1))
+    {
+        if(dec == 0) Console.Write("0");
+        if(dec == 1) Console.Write("1");
+    }
+    else 
+    {
+        f = 0;
+        int d = dec / 2;
+        int o = dec % 2;
+        s = s + o.ToString();
+        // Console.Write(s);
+        if (d > 1)
+        {
+            Dectobin(d, f, s);
+        }
+        else 
+        {
+            s = s + "1";
+            char[] arr = s.ToCharArray();
+            Array.Reverse(arr);
+            Console.WriteLine(arr);
+        }
+    }
+}
