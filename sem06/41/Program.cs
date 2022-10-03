d9119cefecce9e2eb39909158e0b4f0f2c405f27
zayha <5350061@mail.ru>
2022-10-03 23:57:32 +0300

    add task 41 and 43

---
+++ sem06/41/Program.cs
@@ -0,0 +1,26 @@
+﻿int MFu(int m = -1, int count = 0)
+{
+    if (m == -1)
+    {
+        Console.Write("Введите счисло считываний М: ");
+        m = Math.Abs(int.Parse(Console.ReadLine()!));
+    }
+    if (m == 0)
+    {
+        return count;
+    }
+    Console.Write("Введите число: ");
+    int x = int.Parse(Console.ReadLine()!);
+    if (x > 0)
+    {
+        return MFu(--m, ++count);
+    }
+    else 
+    {
+        return MFu(--m, count);
+    }
+    
+}
+    
+Console.Clear();
+Console.WriteLine(MFu());
---
+++ sem06/43/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+Console.WriteLine("*******************************");
+Console.Write("Прямая 1: y = k1 * x + b1\n");
+Console.Write("Прямая 2: y = k2 * x + b2\n");
+Console.WriteLine("*******************************");
+
+Console.Write("Введите значения k1 b1 через пробел, пример - \"5 2\" : ");
+double[] param1 = Console.ReadLine()!.Split(' ').Select(e => Convert.ToDouble(e)).ToArray();
+Console.Write("Введите значения k2 b2 через пробел, пример - \"9 4\" : ");
+double[] param2 = Console.ReadLine()!.Split(' ').Select(e => Convert.ToDouble(e)).ToArray();
+Console.WriteLine("*******************************");
+
+if (param1[0] != param2[0])
+{
+    double x = (param2[1] - param1[1]) / (param1[0] - param2[0]);
+    double y = param1[0] * x + param1[1];   
+    Console.WriteLine($"Точка пересечения ({x} , {y})");
+}
+else
+{
+    Console.WriteLine($"Некорректные параметры k1 и k2, точка пересечения не найдена!");
+}
+Console.WriteLine("*******************************");
