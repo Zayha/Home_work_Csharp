1f037607275b53b3b261433fc94b2a168c14a03e
zayha <5350061@mail.ru>
2022-10-14 19:15:10 +0300

    fix task 72

--- sem09/72/Program.cs
+++ sem09/72/Program.cs
@@ -1,6 +1,7 @@
 ﻿/*
 Доп задача(72)
-Заданы 2 массива: info и data. В массиве info хранятся двоичные представления нескольких чисел (без разделителя). В массиве data хранится информация о количестве бит, которые занимают числа из массива info. Напишите программу, которая составит массив десятичных представлений чисел массива data с учётом информации из массива info.
+Заданы 2 массива: info и data. В массиве info хранятся двоичные представления нескольких чисел (без разделителя). В массиве data хранится информация о количестве бит, 
+которые занимают числа из массива info. Напишите программу, которая составит массив десятичных представлений чисел массива data с учётом информации из массива info.
 входные данные:
 
 data = {0, 1, 1, 1, 1, 0, 0, 0, 1 } 
@@ -11,13 +12,15 @@ info = {2, 3, 3, 1 }
 
 int[] data = { 0, 1, 1, 1, 1, 0, 0, 0, 1 };
 int[] info = { 2, 3, 3, 1 };
-int[] outdata = {1, 7, 0, 1};
+int[] outdata = { 1, 7, 0, 1 };
+int[] result = new int[info.Length]; // выходной массив
 
 Console.Clear();
 string[] array = GetArray(data, info);
 for (int i = 0; i < info.Length; i++)
-{
-    Console.Write(BinToDec(array[i]));
+{    
+    result[i] = BinToDec(array[i]);
+    Console.Write(result[i]);
     if (i < info.Length - 1)
     {
         Console.Write(", ");
@@ -32,12 +35,12 @@ void TestOutData(int[] data, int[] info, int[] outdata)
     int check = 0;
     for (int i = 0; i < info.Length; i++)
     {
-        if(BinToDec(GetArray(data, info)[i]) != outdata[i])
+        if (BinToDec(GetArray(data, info)[i]) != outdata[i])
         {
-            check +=1;
+            check += 1;
         }
     }
-    if(check > 0) Console.WriteLine("\nПолученные данные НЕ соответствуют проверочным!");
+    if (check > 0) Console.WriteLine("\nПолученные данные НЕ соответствуют проверочным!");
     else Console.WriteLine("\nПолученные данные соответствуют проверочным");
 }
 
